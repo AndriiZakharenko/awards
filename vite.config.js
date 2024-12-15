e3d274548fd83e136f511d2b97aa0842fa7d0a62
Andrii Zakharenko <andriizakharenko@gmail.com>
2024-12-15 15:17:47 +0200

    feat: config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/awards/",
+  base: "/awards",
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
